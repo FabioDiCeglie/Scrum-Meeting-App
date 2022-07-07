a3a9b0a629c3b07c9e30fb4a40a102339f8cd0a0
FabioDiCeglie <dcfabio96@gmail.com>
2022-07-07 13:35:14 +0200

    added the first modifiers and the visual appearing of the app

--- Shared/MeetingView.swift
+++ Shared/MeetingView.swift
@@ -2,12 +2,31 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading){
+                Text("Seconds Elapsed")
+                        .font(.caption)
+                Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                Text("Ciao")
+                        .font(.caption)
+                Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
     }
 }
 
+
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        Group {
+       
+            MeetingView()
+        }
     }
 }
