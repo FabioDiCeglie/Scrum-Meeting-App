a618762b3c0df1469b88b364273ff62304aa4f9c
FabioDiCeglie <dcfabio96@gmail.com>
2022-07-07 14:05:49 +0200

    adding accessibility on the applications and footer

--- Shared/MeetingView.swift
+++ Shared/MeetingView.swift
@@ -11,13 +11,26 @@ struct MeetingView: View {
                 Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
-                VStack(alignment: .trailing){
+                VStack(alignment: .center){
                 Text("Ciao")
                         .font(.caption)
                 Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                                   Image(systemName: "forward.fill")
+                               }
+            }
+            
         }
+        .padding()
     }
 }
 
@@ -25,7 +38,6 @@ struct MeetingView: View {
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-       
             MeetingView()
         }
     }
